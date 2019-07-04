8a948634fcdf669df1eaa83c1ffa73d48de52815
josemak25 <amakirij@gmail.com>
2019-07-04 15:37:21 +0100

    add login page to page

--- src/components/Registration/Login.jsx
+++ src/components/Registration/Login.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.css';
 
-import loginImg from '../../img/profile.png';
+import loginImg from '../../img/loginProfile.png';
+import signUpImg from '../../img/signUpProfile.png';
 
 const Login = () => {
+  const [signUp, setSignUp] = useState(false);
+
+  const handleChange = () => {
+    setSignUp(!signUp);
+    console.log(signUp);
+  };
+
+  if (signUp) {
+    const signUpStyle = {
+      label: { marginBottom: '0' },
+      input: { marginBottom: '0.5rem' }
+    };
+    return (
+      <div className={styles.container}>
+        <section className={styles.header}>
+          <div className={styles.logo}>
+            <span>C</span>U
+          </div>
+          <div className={styles.signIn}>SIGN UP</div>
+        </section>
+        <section className={styles.form}>
+          <form>
+            <div className={styles.formImg}>
+              <img src={signUpImg} alt="sign-in" />
+            </div>
+            <div className={styles.formField}>
+              <label style={signUpStyle.label}>Username</label>
+              <input type="text" style={signUpStyle.input} />
+              <label style={signUpStyle.label}>Email</label>
+              <input type="text" style={signUpStyle.input} />
+              <label style={signUpStyle.label}>Password</label>
+              <input type="text" style={signUpStyle.input} />
+              <button style={signUpStyle.input}>Submit</button>
+              <div className={styles.signUp} onClick={handleChange}>
+                Sign in
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container}>
       <section className={styles.header}>
@@ -23,7 +67,7 @@ const Login = () => {
             <label>Password</label>
             <input type="text" />
             <button>Submit</button>
-            <div className={styles.signUp} onClick={e => console.log(e)}>
+            <div className={styles.signUp} onClick={handleChange}>
               Sign up
             </div>
           </div>
